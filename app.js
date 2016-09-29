6c72e5ef4a4b43f35eb70acefa19249e8cc9500e
Britt Anne Sellers <basellers1@yahoo.com>
2016-09-28 17:31:28 -0700

    final Day 7 lab

--- app.js
+++ app.js
@@ -5,115 +5,121 @@
 //SET UP OUR data
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
 var allLocations = [];
 var salesDataTable = document.getElementById('salesdata');
 
-function CookieStand(minCustomers, maxCustomers, averageCookiesPerCust, locationName, htmlID) {
-  this.minCustomers = minCustomers;
-  this.maxCustomers = maxCustomers;
-  this.averageCookiesPerCust = averageCookiesPerCust;
-  this.totalDailyCookies = 0;
-  this.totalCookiesPerHour = [];
-  this.randomCustomersPerHour = [];
+function CookieStand(locationName, minCustPerHour, maxCustPerHour, avgCookiesPerCust) {
   this.locationName = locationName;
-  this.htmlID = htmlID;
-  this.calculateRandCustHour = function () {
-    for(var i = 0; i < hours.length; i++) {
-      this.randomCustomersPerHour.push(Math.ceil(Math.random() * (this.maxCustomers - this.minCustomers)) + this.minCustomers);
+  this.minCustPerHour = minCustPerHour;
+  this.maxCustPerHour = maxCustPerHour;
+  this.avgCookiesPerCust = avgCookiesPerCust;
+  this.randomCustPerHour = [];
+  this.totalCookiesSoldEachHour = [];
+  this.totalDailyCookieSales = 0;
+  this.calcRandomCustPerHour = function() {
+    for (var i = 0; i < hours.length; i++){
+      this.randomCustPerHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
+    }
+    // return console.log('Random hourly customers have been calculated');
+  };
+  this.calcTotalCookiesSoldEachHour = function() {
+    this.calcRandomCustPerHour();
+    for (var i = 0; i < hours.length; i++) {
+      this.totalCookiesSoldEachHour.push(Math.ceil(this.randomCustPerHour[i] * this.avgCookiesPerCust));
+      this.totalDailyCookieSales += this.totalCookiesSoldEachHour[i];
     }
   };
-  this.calculateTotalDailyCookies = function () {
-    this.calculateRandCustHour();
-    for(var j = 0; j < hours.length; j++) {
-      this.totalCookiesPerHour.push(Math.ceil(this.randomCustomersPerHour[j] * this.averageCookiesPerCust));
-      this.totalDailyCookies += this.totalCookiesPerHour[j];
-    }
-  };
-
   this.render = function() {
-    this.calculateTotalDailyCookies();
+    this.calcTotalCookiesSoldEachHour();
 
     var rowElement = document.createElement('tr');
 
-    var headerCell = document.createElement('th');
+    var headerCell = document.createElement('th'); // far left cell
     headerCell.textContent = this.locationName;
     rowElement.appendChild(headerCell);
 
-    for (var i = 0; i < hours.length; i++){
-      headerCell = document.createElement('td');
-      headerCell.textContent = hours[i];
+    for (var i = 0; i < hours.length; i++) { //all the ones in the middle
+      var dataCell = document.createElement('td');
+      dataCell.textContent = this.totalCookiesSoldEachHour[i];
       rowElement.appendChild(dataCell);
     }
-    headerCell = document.createElement('td');
-    headerCell.textContent = this.locationName;
+
+    dataCell = document.createElement('td'); // far right cell
+    dataCell.textContent = this.totalDailyCookieSales;
     rowElement.appendChild(dataCell);
 
     salesDataTable.appendChild(rowElement);
+
   };
-
+  allLocations.push(this);
 }
-var stores = [];
 
-new CookieStand(23, 65, 6.3, 'First and Pike', 'first-and-pike');
-new CookieStand(3, 24, 1.2, 'Seatac Airport', 'seatac-airport');
-new CookieStand(11, 38, 3.7, 'Seattle Center', 'seattle-center');
-new CookieStand(20, 38, 2.3, 'Capitol Hill', 'capitol-hill');
-new CookieStand(2, 16, 4.6, 'Alki', 'alki');
+new CookieStand('Alki', 2, 16, 4.6);
+new CookieStand('Seatac Airport', 3, 24, 1.2);
+new CookieStand('Seattle Center', 11, 38, 3.7);
+new CookieStand('Capitol Hill', 20, 38, 2.3);
+new CookieStand('First and Pike', 23, 65, 6.3);
 
-//-----------------------------------------------
-//------------------------------------------------
-//DEFINE OUR ACTIONS
-function makeHeaderRow(){
+// ++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++
+// DEFINE OUR ACTIONS
+
+function makeHeaderRow() {
   var rowElement = document.createElement('tr');
 
   var headerCell = document.createElement('th');
-  headerCell.textContent = 'Hours Open';
+  headerCell.textContent = 'Locations';
   rowElement.appendChild(headerCell);
 
-  for (var i = 0; i < hours.length; i++){
+  for (var i = 0; i < hours.length; i++) {
     headerCell = document.createElement('th');
     headerCell.textContent = hours[i];
     rowElement.appendChild(headerCell);
-
   }
-  salesDataTable.appendChild(rowElement);
 
-}
-
-function makeFooterRow(){
-  var rowElement = document.createElement('tr');
-
-  var headerCell = document.createElement('th');
-  headerCell.textContent = this.locationName;
+  headerCell = document.createElement('th');
+  headerCell.textContent = 'Location Total';
   rowElement.appendChild(headerCell);
 
-  var allLocations = 0;
-  for (var i = 0; i < hours.length; i++) {
-    var total = 0;
-    for (var j = 0; i < allLocations.length; i++) {
-      total += allLocations[j].totalCookiesPerHour[i];
-    }
-    headerCell = document.createElement('th');
-    headerCell.textContent = this.locationName;
-
-
-  }
+  salesDataTable.appendChild(rowElement);
 }
 
-function renderAllLocations(){
-  for (var i = 0; i < stores.length; i++) {
+function renderAllLocations() {
+  for (var i = 0; i < allLocations.length; i++) {
     allLocations[i].render();
   }
 }
-//-----------------------------------------------
-//------------------------------------------------
-//this IS WHERE OUR CODE EXECUTES
 
-makeHeaderRow();
-//list the time across the top
+function makeFooterRow() {
+  var rowElement = document.createElement('tr');
 
-renderAllLocations();
-//loop through the array of objects representing eadh store location and call the render method that each one has, so as to display data in each row
-makeFooterRow();
+  var headerCell = document.createElement('th'); //far left
+  headerCell.textContent = 'Hourly Totals';
+  rowElement.appendChild(headerCell);
 
-//make a row acrosss the bottom of the table that sums
+  var dailyTotalAllLocations = 0;
+  for (var i = 0; i < hours.length; i++) {
+    var total = 0;
+    for (var j = 0; j < allLocations.length; j++) { //allLocations
+      total += allLocations[j].totalCookiesSoldEachHour[i];
+    }
+    headerCell = document.createElement('th');
+    headerCell.textContent = total;
+    rowElement.appendChild(headerCell);
+    dailyTotalAllLocations += total;
+  }
+
+  headerCell = document.createElement('th');
+  headerCell.textContent = dailyTotalAllLocations;
+  rowElement.appendChild(headerCell);
+
+  salesDataTable.appendChild(rowElement);
+}
+// ++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++
+// THIS IS WHERE THE CODE ACTUALLY EXECUTES
+
+makeHeaderRow(); // list the times across the top
+renderAllLocations(); // loop through the array of objects representing each store location and call the .render() method that each one has, so as to display its data in a row
+makeFooterRow(); // make a row across the bottom of the table that sums each hour's total across all locations
